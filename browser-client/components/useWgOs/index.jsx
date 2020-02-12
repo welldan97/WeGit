7ce59a7ad73dd760ce0834abad7a8ad52efdbfd5
Dmitry Yakimov <welldan97@gmail.com>
2020-02-12 16:57:31 +0000

    Fix bug regarding user ids mismatch

--- browser-client/components/useWgOs/index.jsx
+++ browser-client/components/useWgOs/index.jsx
@@ -201,7 +201,10 @@ export default function useWgOs() {
     setCurrentUser(user);
     wgOs.saveCurrentUser(user);
     //
-    Cookies.set('weGit', { config: nextConfig, currentUser: user });
+    Cookies.set('weGit', {
+      config: nextConfig,
+      currentUser: { userName: user.userName },
+    });
     if (!_isEqual(config, nextConfig)) {
       location.reload(true);
     }
